1bb143b3fcbaa069d3b740f8f65d52ce7ecafaa5
Rachit Chahar <202151123@iiitvadodara.ac.in>
2023-07-05 02:37:35 +0530

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,16 @@
 import './App.css';
 import Login from './Login'
 import Home from './Home'
-import "@rainbow-me/rainbowkit/styles.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { sepolia } from "wagmi/chains";
-import { publicProvider } from "wagmi/providers/public";
-
-const { chains, publicClient } = configureChains([sepolia], [publicProvider()]);
-
-const { connectors } = getDefaultWallets({
-  appName: "nft",
-  projectId: "f1881124fe9b66e89480103e523c0ec9",
-  chains,
-});
-
-const config = createConfig({
-  autoConnect: true,
-  connectors,
-  publicClient,
-});
 
 function App() {
   return (
-    <WagmiConfig config={config}>
-      <RainbowKitProvider chains={chains}>
         <BrowserRouter>
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/login" element={<Login />} />
           </Routes>
         </BrowserRouter>
-      </RainbowKitProvider>
-    </WagmiConfig>
   );
 }
 
